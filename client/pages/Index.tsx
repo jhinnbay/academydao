3a01b46084ba91f627ed99f502058e569e88453c
Quinn <Home@imac.mynetworksettings.com>
2025-09-05 20:03:03 -0400

    Move transparent background to wrapper div instead of individual components
    - Apply bg-transparent to the scale-90 wrapper div
    - Remove bg-transparent from Avatar, Name, and Badge components
    - Cleaner styling approach

--- client/pages/Index.tsx
+++ client/pages/Index.tsx
@@ -570,14 +570,14 @@ export default function Index() {
 
               {/* OnchainKit Identity */}
               {isConnected && wagmiAddress && (
-                <div className="scale-90">
+                <div className="scale-90 bg-transparent">
                   <Identity
                     address={wagmiAddress}
                     schemaId="0xf8b05c79f090979bf4a80270aba232dff11a10d9ca55c4f88de95317970f0de9"
                   >
-                    <Avatar className="bg-transparent" />
-                    <Name className="bg-transparent">
-                      <Badge className="bg-transparent" />
+                    <Avatar />
+                    <Name>
+                      <Badge />
                     </Name>
                   </Identity>
                 </div>
