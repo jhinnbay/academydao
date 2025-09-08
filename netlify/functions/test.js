4ca2c0a9e8d54611ad5225a291dc4c84069b0e5f
Quinn <Home@imac.mynetworksettings.com>
2025-09-07 20:15:24 -0400

    test: Add simple test function to debug ES module issue

---
+++ netlify/functions/test.js
@@ -0,0 +1,11 @@
+// Simple test function
+export const handler = async (event, context) => {
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ message: 'Test function working!' }),
+  };
+};
